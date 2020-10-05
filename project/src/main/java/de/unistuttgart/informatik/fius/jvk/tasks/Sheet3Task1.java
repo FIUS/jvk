09de9b62a1cf146c60806c333c5aa8664155ae2a
Jannik <jannik.graef@gmail.com>
2020-10-05 14:10:50 +0200

    Added more Neos
    Added Neos are used in diffrent Task so you can finish one Task without
    first completing all prior Task.

--- project/src/main/java/de/unistuttgart/informatik/fius/jvk/tasks/Sheet3Task1.java
+++ project/src/main/java/de/unistuttgart/informatik/fius/jvk/tasks/Sheet3Task1.java
@@ -2,7 +2,6 @@ package de.unistuttgart.informatik.fius.jvk.tasks;
 
 import java.util.Iterator;
 
-
 import de.unistuttgart.informatik.fius.icge.simulation.Position;
 import de.unistuttgart.informatik.fius.icge.simulation.Simulation;
 import de.unistuttgart.informatik.fius.icge.simulation.entity.CollectableEntity;
@@ -15,13 +14,25 @@ import de.unistuttgart.informatik.fius.jvk.provided.shapes.*;
 
 public class Sheet3Task1 implements Task {
     
+    
     @Override
     public void run(Simulation sim) {
         // Implementation here
-        Neo neo = new Neo();
+        
         PlayfieldModifier pm = new PlayfieldModifier(sim.getPlayfield());
+        
+        // use this neo for b), c) and d) and e)
+        Neo neo = new Neo();
         pm.placeEntityAt(neo, new Position(0, 0));
         
+        Neo neoF = new Neo();
+        pm.placeEntityAt(neoF, new Position(0, 2));
+        
+        Neo neoG = new Neo();
+        pm.placeEntityAt(neoG, new Position(0, 4));
+        
+        Neo neoH = new Neo();
+        pm.placeEntityAt(neoH, new Position(0, 6));
         
     }
     
