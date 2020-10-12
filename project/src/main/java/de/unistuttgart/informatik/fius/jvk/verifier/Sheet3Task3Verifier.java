a2a1d7e2969469136787bb07a1f9728b644d7e4e
Jannik <jannik.graef@gmail.com>
2020-10-12 18:27:41 +0200

    added Sheet3Task3Verifier

---
+++ project/src/main/java/de/unistuttgart/informatik/fius/jvk/verifier/Sheet3Task3Verifier.java
@@ -0,0 +1,18 @@
+/*
+ * This source file is part of the FIUS JVK 2019 project.
+ * For more information see github.com/FIUS/JVK-2019
+ *
+ * Copyright (c) 2019 the FIUS JVK 2019 project authors.
+ * 
+ * This software is available under the MIT license.
+ * SPDX-License-Identifier:    MIT
+ */
+package de.unistuttgart.informatik.fius.jvk.verifier;
+
+/**
+ * TODO: Description
+ * @author Jannik
+ */
+public class Sheet3Task3Verifier {
+    
+}
