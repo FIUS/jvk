31edc5995535510d06a95dbed90ce7d345dc6516
Jannik Graef <jannik.graef@gmail.com>
2020-10-16 03:24:15 +0200

    subtask b) and c)

--- project/src/main/java/de/unistuttgart/informatik/fius/jvk/verifier/Sheet3Task3Verifier.java
+++ project/src/main/java/de/unistuttgart/informatik/fius/jvk/verifier/Sheet3Task3Verifier.java
@@ -1,18 +1,106 @@
-/*
- * This source file is part of the FIUS JVK 2019 project.
- * For more information see github.com/FIUS/JVK-2019
- *
- * Copyright (c) 2019 the FIUS JVK 2019 project authors.
- * 
- * This software is available under the MIT license.
- * SPDX-License-Identifier:    MIT
- */
 package de.unistuttgart.informatik.fius.jvk.verifier;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
+import de.unistuttgart.informatik.fius.icge.simulation.Position;
+import de.unistuttgart.informatik.fius.icge.simulation.Simulation;
+import de.unistuttgart.informatik.fius.icge.simulation.TaskVerifier;
+import de.unistuttgart.informatik.fius.icge.simulation.actions.*;
+import de.unistuttgart.informatik.fius.icge.simulation.tools.PlayfieldModifier;
+import de.unistuttgart.informatik.fius.icge.ui.TaskInformation;
+import de.unistuttgart.informatik.fius.icge.ui.TaskVerificationStatus;
+import de.unistuttgart.informatik.fius.jvk.provided.BasicTaskInformation;
+import de.unistuttgart.informatik.fius.jvk.provided.entity.Coin;
+import de.unistuttgart.informatik.fius.jvk.provided.entity.Neo;
+import de.unistuttgart.informatik.fius.jvk.provided.entity.Wall;
+import de.unistuttgart.informatik.fius.jvk.provided.shapes.Line;
+import de.unistuttgart.informatik.fius.jvk.provided.shapes.Rectangle;
+import de.unistuttgart.informatik.fius.jvk.tasks.Sheet3Task1;
+
+
 /**
- * TODO: Description
- * @author Jannik
+ *
+ * @author Jannik Graef
  */
-public class Sheet3Task3Verifier {
+public class Sheet3Task3Verifier implements TaskVerifier {
+    
+    private BasicTaskInformation task;
+    
+    private BasicTaskInformation taskA = new BasicTaskInformation(
+            "a) Select this task", "Select this task.", TaskVerificationStatus.SUCCESSFUL
+    );
+    private BasicTaskInformation taskC = new BasicTaskInformation("c)", "Walk until you find a coin.");
+    private BasicTaskInformation taskD = new BasicTaskInformation(
+            "d)", "when on a field with exactly one coin, pick up the coin and turn right."
+    );
+    private BasicTaskInformation taskE = new BasicTaskInformation(
+            "e)", "when on a field with more than one coin, pick up a coin and turn right."
+    );
+    private BasicTaskInformation taskF = new BasicTaskInformation("f)", "Turn around when facing a wall and not standing on a coin.");
+    private BasicTaskInformation taskG = new BasicTaskInformation("g)", "Walk until you have collected 20 coins or walked 10 steps");
+    private BasicTaskInformation taskH = new BasicTaskInformation("h)", "complete all other subtasks.");
+    
+    private ActionLog  actionLog;
+    private Simulation sim;
+    
+    public Sheet3Task3Verifier() {
+        
+        //System.gc();
+        //System.out.println("test");
+        List<BasicTaskInformation> subTasks = new ArrayList<>();
+        subTasks.add(this.taskA);
+        subTasks.add(this.taskC);
+        subTasks.add(this.taskD);
+        subTasks.add(this.taskE);
+        subTasks.add(this.taskF);
+        subTasks.add(this.taskG);
+        subTasks.add(this.taskH);
+        this.task = new BasicTaskInformation("Sheet 3 Task 2", "If-Conditions.", subTasks);
+    }
+    
+    @Override
+    public void attachToSimulation(Simulation sim) {
+        this.sim = sim;
+        this.actionLog = sim.getActionLog();
+        //System.out.println("test2");
+    }
+    
+    @Override
+    public void verify() {
+        this.actionLog = this.sim.getActionLog();
+        List<EntityStepAction> stepActions = this.actionLog.getActionsOfType(EntityStepAction.class, true);
+        List<List<Position>> stepPositions = new ArrayList<>();
+        stepActions.forEach(stepAction -> stepPositions.add(Arrays.asList(new Position[] { stepAction.from(), stepAction.to() })));
+        
+        List<BasicTaskInformation> subTasks = new ArrayList<>();
+        subTasks.add(this.taskA);
+        subTasks.add(this.taskC);
+        subTasks.add(this.taskD);
+        subTasks.add(this.taskE);
+        subTasks.add(this.taskF);
+        subTasks.add(this.taskG);
+        subTasks.add(this.taskH);
+        this.task = this.task.updateSubTasks(subTasks);
+        
+    }
+    
+    private int getNumberOfCoinsAtPosition(int x, int y) {
+        return getNumberOfCoinsAtPosition(new Position(x, y));
+    }
+    
+    private int getNumberOfCoinsAtPosition(Position pos) {
+        return this.sim.getPlayfield().getEntitiesOfTypeAt(pos, Coin.class, true).size();
+    }
+    
+    @Override
+    public TaskInformation getTaskInformation() {
+        return this.task;
+    }
     
 }
