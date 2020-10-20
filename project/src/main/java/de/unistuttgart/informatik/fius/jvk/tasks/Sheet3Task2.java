3beb1926f4222da852059627eabf71df7c712e24
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2020-10-20 18:31:39 +0200

    Rework sheet 3 Task 2
    Made algorithm more complex
    Made field more random
    Made verifier more complex/ugly
    Add infobox for endless loops (merged with break)
    Add more examples for comparing objects with == and .equal

--- project/src/main/java/de/unistuttgart/informatik/fius/jvk/tasks/Sheet3Task2.java
+++ project/src/main/java/de/unistuttgart/informatik/fius/jvk/tasks/Sheet3Task2.java
@@ -32,6 +32,8 @@ public class Sheet3Task2 implements Task{
         PlayfieldModifier pm = new PlayfieldModifier(sim.getPlayfield());
         Neo neo = new Neo();
         pm.placeEntityAt(neo, new Position(0, 0));
+
+        // implement the task here
         
     }
     
@@ -48,19 +50,41 @@ public class Sheet3Task2 implements Task{
     
     private void preparePlayingField(Simulation sim) {
         PlayfieldModifier pm = new PlayfieldModifier(sim.getPlayfield());
-        pm.placeEntityAt(new Coin(), new Position(4, 0));
-        pm.placeMultipleEntitiesAt(new CoinFactory(), 3, new Position(4, 4));
-        pm.placeEntityAt(new Coin(), new Position(12, 4));
-
+        
         Shape walls = new UnionShape(
             new Line(new Position(13, 3), new Position(13, 6)),
             new Line(new Position(-10, 7), new Position(13, 7)),
             new Line(new Position(14,3), new Position(30,3))
         );
         pm.placeEntityAtEachPosition(new WallFactory(), walls);
+            
+            
+        Random r = new Random();
+
+        Integer clockwiseRounds = r.nextInt(2);
+
+        if (clockwiseRounds > 0) {
+            pm.placeEntityAt(new Coin(), new Position(2, 0));
+            pm.placeEntityAt(new Coin(), new Position(2, 5));
+            pm.placeEntityAt(new Coin(), new Position(-1, 5));
+            pm.placeEntityAt(new Coin(), new Position(-1, 0));
+        }
+
+        pm.placeEntityAt(new Coin(), new Position(4, 0));
+
+        Integer counterclockwiseRounds = r.nextInt(3) + 1;
+
+        pm.placeMultipleEntitiesAt(new CoinFactory(), counterclockwiseRounds + 1, new Position(4, 4));
+        pm.placeMultipleEntitiesAt(new CoinFactory(), counterclockwiseRounds, new Position(7, 4));
+        pm.placeMultipleEntitiesAt(new CoinFactory(), counterclockwiseRounds + 2, new Position(7, -1));
+        pm.placeMultipleEntitiesAt(new CoinFactory(), counterclockwiseRounds + 1 + r.nextInt(3), new Position(4, -1));
+
+        
+        pm.placeMultipleEntitiesAt(new CoinFactory(), 3, new Position(4, 4));
+        pm.placeEntityAt(new Coin(), new Position(12, 4));
 
         pm.placeEntityAt(new Coin(), new Position(12, 2));
-        pm.placeMultipleEntitiesAt(new CoinFactory(), 7, new Position(new Random().nextInt(3)+13, 2));
+        pm.placeMultipleEntitiesAt(new CoinFactory(), 9, new Position(new Random().nextInt(3)+13, 2));
     }
     
 }
