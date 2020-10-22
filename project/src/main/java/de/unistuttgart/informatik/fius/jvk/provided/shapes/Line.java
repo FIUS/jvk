b8e375e11866bb2b80c35ba2d593f65c5e20f788
ruffdd <david.ruff@live.de>
2020-10-22 17:28:54 +0200

    improve Line shape
    the Line shape can now go in all directions

--- project/src/main/java/de/unistuttgart/informatik/fius/jvk/provided/shapes/Line.java
+++ project/src/main/java/de/unistuttgart/informatik/fius/jvk/provided/shapes/Line.java
@@ -3,63 +3,52 @@ package de.unistuttgart.informatik.fius.jvk.provided.shapes;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+import java.lang.Math;
 import de.unistuttgart.informatik.fius.icge.simulation.Position;
 
+
 /**
- * A finite length line shape that is always parallel to the coordinate axes.
+ * A finite length line shape.
  */
 public class Line implements Shape {
-
+    
     private List<Position> points;
-
+    
     /**
      * Create a new line by specifying start and end position of the line.
      *
-     * @param start the start of the line
-     * @param end the end of the line; may not differ from the line start in both x and y direction
+     * @param start
+     *     the start of the line
+     * @param end
+     *     the end of the line; may not differ from the line start in both x and y direction
      */
     public Line(Position start, Position end) {
-        Integer deltaX = start.getX() - end.getX();
-        Integer deltaY = start.getY() - end.getY();
-
-        if (deltaX != 0 && deltaY != 0) {
-            throw new IllegalArgumentException("Only lines parallel to the grid are supported.");
-        }
-
+        Integer deltaX = end.getX() - start.getX();
+        Integer deltaY = end.getY() - start.getY();
+        Float slope; //slope needs to have digits after the . ,so Float is used
+        
         List<Position> points = new ArrayList<>();
-        points.add(start);
-
-        Position currentPos = start;
-        while (deltaX != 0 || deltaY != 0) {
-            if (deltaX > 0) {
-                currentPos = new Position(currentPos.getX() - 1, currentPos.getY());
-                points.add(currentPos);
-                deltaX -= 1;
-            }
-            if (deltaX < 0) {
-                currentPos = new Position(currentPos.getX() + 1, currentPos.getY());
-                points.add(currentPos);
-                deltaX += 1;
-            }
-            if (deltaY > 0) {
-                currentPos = new Position(currentPos.getX(), currentPos.getY() - 1);
-                points.add(currentPos);
-                deltaY -= 1;
-            }
-            if (deltaY < 0) {
-                currentPos = new Position(currentPos.getX(), currentPos.getY() + 1);
-                points.add(currentPos);
-                deltaY += 1;
+        
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            slope =  deltaY.floatValue() / deltaX.floatValue();
+            for (Integer x = start.getX(); (deltaX > 0) ? x <= end.getX() : x >= end.getX(); x += ((deltaX > 0 ? 1 : -1))) {
+                Integer y = Math.round(x.floatValue() * slope + start.getX());
+                points.add(new Position(x, y));
             }
         }
-
+        else{
+            slope =  deltaX.floatValue() / deltaY.floatValue();
+            for(Integer y = start.getY(); (deltaY > 0) ? y <= end.getY() : y >= end.getY(); y += ((deltaY > 0 ? 1 : -1))){
+                Integer x = Math.round(y.floatValue() * slope + start.getY());
+                points.add(new Position(x, y));
+            }
+        }
         this.points = points;
     }
-
+    
     @Override
     public Iterator<Position> iterator() {
         return this.points.iterator();
     }
-
+    
 }
